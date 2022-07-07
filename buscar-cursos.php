5d59e9abbdc9b941bdfc2530c5c3028857a3eba7
“Paulo <corazza.paulovinicius@gmail.com>
2022-07-07 14:18:37 -0300

    crawler

---
+++ buscar-cursos.php
@@ -0,0 +1,16 @@
+<?php
+
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+$client =  new Client();
+$response = $client->request('GET','https://alura.com.br/cursos-online-programacao/php');
+$html = $response->getBody();
+
+$crawler = new Crawler();
+$crawler->addHtmlContent($html);
+$cursos = $crawler->filter('span.card-curso__nome');
+
+foreach($cursos as $curso){
+    echo $curso->textContent . PHP_EOL;
+}
